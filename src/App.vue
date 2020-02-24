3f0b4ec71bb757d306c900311e3b504912346963
Daniel <allegro.danielsobieraj@gmail.com>
2020-02-24 22:17:58 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,45 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-container
+                fill-height>
+            <v-row justify="center">
+                <v-col
+                        cols="6">
+                </v-col>
+            </v-row>
+            <v-row justify="center">
+                <v-col
+                        cols="6">
+                    <v-text-field
+                            autofocus
+                            autocomplete="off"
+                            placeholder="What need to be done?"
+                            v-model="newTodo"
+                            @keyup.enter="addTodo">
+                    </v-text-field>
+                    <v-btn
+                            @click="addTodo">
+                        Add
+                    </v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+    export default {
+        name: 'App',
+        data() {
+            return {
+                newTodo: [],
+            };
+        },
+        methods: {
+            addTodo() {
+                this.newTodo.push({value: ''});
+            }
+        }
+    };
 </script>
