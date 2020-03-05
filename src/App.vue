3c849d07004581a77e37bc612558f3528a50d207
Daniel <allegro.danielsobieraj@gmail.com>
2020-03-05 14:36:27 +0100

    Commit

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,7 @@
                         style="border: 1px solid grey">
                     <div :key="todo.id"
                          class="d-flex justify-space-between align-center"
-                         v-for="(todo, index) in todos">
+                         v-for="(todo, index) in todosFiltered">
                         <div class="d-flex align-center">
                             <v-checkbox color="success" v-model="todo.completed"></v-checkbox>
                             <v-list>
@@ -63,13 +63,34 @@
                     <div class="d-flex align-center justify-space-between">
                         <div class="d-flex align-center">
                             <v-checkbox
-                                    :inputValue="!anyRemaining"
+                                    :value="!anyRemaining"
                                     @change="checkAllTodos"
                                     color="success"
+                                    label="Check All"
                             />
-                            Check All
                         </div>
                         <div>{{ remaining }} items left</div>
+                    </div>
+                    <div class="d-flex justify-space-around align-center">
+                        <v-btn
+                                @click="filter = 'all'"
+                                small>All
+                        </v-btn>
+                        <v-btn
+                                @click="filter = 'active'"
+                                small>Active
+                        </v-btn>
+                        <v-btn
+                                @click="filter = 'completed'"
+                                small>Completed
+                        </v-btn>
+                        <v-fade-transition>
+                            <v-btn
+                                    @click="clearCompleted"
+                                    small
+                                    v-if="showClearCompletedButton">Clear Completed
+                            </v-btn>
+                        </v-fade-transition>
                     </div>
                 </v-col>
             </v-row>
@@ -85,6 +106,7 @@
                 newTodo: '',
                 idForTodo: 3,
                 beforeEditCache: '',
+                filter: 'all',
                 todos: [
                     {
                         'id': 1,
@@ -134,7 +156,12 @@
                 todo.editing = false;
             },
             checkAllTodos() {
-                this.todos.forEach((todo) => todo.completed = event.target.inputValue)
+                // TO DOKONCZENIA!
+                this.todos.forEach((todo) => todo.completed = event.target);
+                // this.todos.forEach((todo) => todo.completed = event.target.inputValue);
+            },
+            clearCompleted() {
+                this.todos = this.todos.filter(todo => !todo.completed)
             }
         },
         computed: {
@@ -143,7 +170,19 @@
             },
             anyRemaining() {
                 return this.remaining != 0
-
+            },
+            todosFiltered() {
+                if (this.filter == 'all') {
+                    return this.todos
+                } else if (this.filter == 'active') {
+                    return this.todos.filter(todo => !todo.completed)
+                } else if (this.filter == 'completed') {
+                    return this.todos.filter(todo => todo.completed)
+                }
+                return this.todos
+            },
+            showClearCompletedButton() {
+                return this.todos.filter(todo => todo.completed).length > 0
             }
         }
     };
